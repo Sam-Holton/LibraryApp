35a59bdbca7065778d43b5eb433f690437e1efac
Sam <samuel.holton@gmail.com>
2020-11-27 18:55:33 -0500

    Some Code

--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -9,6 +9,51 @@ namespace LibraryApp
 
         }
 
+        public static List<Book> searchIndex()
+        {
+            Console.WriteLine("1. Author");
+            Console.WriteLine("2. Title");
+            Console.Write("Please select one of the above items to search by: ");
+            int userChoice;
+            List<Book> bookSearch = new List<Book>;
+
+            do
+            {
+                string userInput = Console.ReadLine();
+                if (int.TryParse(userInput, out userChoice) && userChoice > 0 && userChoice < 3)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please select either 1 for Author or 2 for Title: ");
+                }
+            } while (true);
+
+            switch (userChoice)
+            {
+                case 1:
+                    {
+                        Console.Write("Please enter in the last name of the Author you wish to search for: ");
+                        string author = Console.ReadLine();
+
+                        foreach (string book in listOfBooks)
+                        {
+                            if (listOfBooks[0] == author) Console.WriteLine(Book.Title);
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Write("Please enter the Title of the book you are searching for: ");
+                        string title = Console.ReadLine();
+
+
+                    }
+            }
+                
+        }
+
         public void AddBookTOList(Book newBook)
         {
             string author = newBook.Author;
